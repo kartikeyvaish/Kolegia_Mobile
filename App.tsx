d6c35fc2076191eed32a3087648a99760a16cac8
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-05 12:56:08 +0530

    :zap: Environment variables configured

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from "react-native";
+
+function App(props) {
+  return <View style={styles.container}></View>;
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
