5b45f0d5531f7287db8a22e29787d327180517c4
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-07 11:40:40 +0530

    :sparkles: Add utils for the application

---
+++ utils/ColorPallete.ts
@@ -0,0 +1,16 @@
+export default {
+    primary: "#FFA001",
+    white: "#FFFFFF",
+    black: "#212121",
+    lightModeText: "#121212",
+    darkModeText: "#FAFAFA",
+    red: "#FF4C58",
+    green: "#4BB543",
+    lightgreen: "#90ee90",
+    warning: "#FFB259",
+    orange: "#FFA001",
+    purple: "#32297C",
+    pinkRed: "#F25767",
+    grey: 'E7E7F7',
+    googleColor: '#4285F4',
+}
---
+++ utils/Helper.ts
@@ -0,0 +1,22 @@
+// Packages imports
+import { Alert, ToastAndroid, AlertButton, AlertOptions } from "react-native"
+
+// Helper function to show toast
+export const ShowToast = (message: string, duration: number = 2000) => {
+    ToastAndroid.show(message, duration)
+}
+
+// Helper function to show Alert boxes
+export const ShowAlert = (title: string, message?: string, buttons?: AlertButton[], options?: AlertOptions) => {
+    Alert.alert(
+        title,
+        message,
+        buttons,
+        options
+    );
+}
+
+// generate a random unique _id
+export const GenerateUniqueID = () => {
+    return Math.floor(Math.random() * Date.now()).toString();
+}
