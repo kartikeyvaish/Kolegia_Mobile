947bb6d77ea7632b2340355bb194f27547e0775c
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-28 14:39:28 +0530

    :sparkles: added new messages

--- constants/Messages.ts
+++ constants/Messages.ts
@@ -1,6 +1,7 @@
 // List of Messages for the application
 const ToastMessages = {
-    SERVER_ERROR_MESSAGE: "Server Error. Please try again later."
+    SERVER_ERROR_MESSAGE: "Server Error. Please try again later.",
+    PasswordResetSuccess: "Password Reset Successfully"
 }
 
 // Exports
