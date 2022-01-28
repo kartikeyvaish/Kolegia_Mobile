ce3768027203be66478206a3556ea56460dc22ba
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-28 14:34:06 +0530

    :wrench: config file

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,16 @@
+// packages imports
 import env_variables from "react-native-config";
 
-const configurations = {
-    ...env_variables,
+// types imports
+import { ConfigProps } from './../types/ConfigTypes';
+
+const { __DEV__, DEV_BASE_URL, PROD_BASE_URL, ...restEnv } = env_variables;
+
+// Prepare the export object
+const configurations: ConfigProps = {
+    baseUrl: __DEV__ === "development" ? DEV_BASE_URL : PROD_BASE_URL,
+    ...restEnv
 }
 
-export default configurations
+// Exports
+export default configurations;
