a6daf3e2ed0b7f74566e4cf95a23afd279860cf6
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-28 14:37:25 +0530

    :lipstick: types configured

--- types/ComponentTypes.ts
+++ types/ComponentTypes.ts
@@ -1,12 +1,18 @@
 // Packages Imports
 import Animated from "react-native-reanimated"
 import { ColorValue, GestureResponderEvent, ImageStyle, KeyboardTypeOptions, OpaqueColorValue, StatusBarStyle, StyleProp, TextProps, TextStyle, ViewStyle } from "react-native";
+import { AVPlaybackSource, AVPlaybackStatusToSet } from "expo-av/build/AV.types";
 
 // AnimatedView Props interface
 export interface AnimatedViewProps extends Animated.AnimateProps<ViewStyle> {
     children: React.ReactNode;
     style?: StyleProp<ViewStyle>;
 }
+
+// AnimatedText Props interface
+export interface AnimatedTextProps
+    extends AppTextProps,
+    Animated.AnimateProps<any> { }
 
 // AppButtonProps interface
 export interface AppButtonProps {
@@ -55,6 +61,7 @@ export interface AppFormFieldProps extends AppTextInputProps {
 export interface AppHeaderBarProps {
     title?: string;
     onIconPress?: () => void;
+    backgroundColor?: ColorValue;
 }
 
 // AppImage interface
@@ -130,6 +137,7 @@ export interface AppTextInputProps extends AppHelperTextProps {
     multiline?: boolean;
     disabled?: boolean;
     showError?: boolean;
+    maxLength?: number;
 }
 
 // AppLoading interface
@@ -139,6 +147,21 @@ export interface AppLoadingProps {
     loadingSize?: number;
     loading?: boolean;
     style?: StyleProp<ViewStyle>;
+}
+
+// Interface for AppModalProps
+export interface AppModalProps {
+    children?: React.ReactNode;
+    isVisible?: boolean;
+    style?: any;
+    backdropColor?: string;
+    backdropOpacity?: number;
+    coverScreen?: boolean;
+    hasBackdrop?: boolean;
+    onBackButtonPress?: () => void;
+    onDismiss?: () => void;
+    useNativeDriver?: boolean;
+    backgroundColor?: string;
 }
 
 // Container Props interface
@@ -174,6 +197,22 @@ export interface AppIconTypes {
     style?: StyleProp<TextStyle>
 }
 
+// ChatKeyboardProps interface
+export interface ChatKeyboardProps {
+    onChangeText?: (text: string) => void;
+    onSubmit?: () => void;
+    value?: string;
+    placeholder?: string;
+    containerStyle?: StyleProp<ViewStyle>;
+    onPickPress?: () => void;
+    onCameraPress?: () => void;
+    showCameraIcon?: boolean;
+    showFileIcon?: boolean;
+    loading?: boolean;
+    backgroundColor?: ColorValue;
+    color?: ColorValue;
+}
+
 // Developers Props interface
 export interface DeveloperCardProps {
     _id?: string | number;
@@ -182,6 +221,28 @@ export interface DeveloperCardProps {
     linkedin_url?: string;
     github_url?: string;
     email_address?: string;
+}
+
+// FileModalProps interface
+export interface FileModalProps extends AppModalProps {
+    // File Props
+    uri?: string;
+    mimeType?: string;
+
+    // Caption Props
+    text?: string;
+    headerTitle?: string;
+
+    // Keyboard Props
+    onChangeText?: (text: string) => void;
+    onSubmit?: () => void;
+    placeholder?: string;
+    loading?: boolean;
+    value?: string;
+    showKeyboard?: boolean;
+
+    // Children
+    children?: any;
 }
 
 // MenuCard Props interface
@@ -209,6 +270,41 @@ export interface MenuCardProps {
     customSuffixIcon?: React.ReactNode;
 }
 
+// Message Props interface
+export interface MessageProps {
+    _id?: string | number;
+    room_id?: string;
+    message?: string;
+    sender_id?: string;
+    message_type?: string;
+    reciever_id?: string;
+    message_datetime?: string;
+    read?: boolean;
+    onMessagePress?: () => void;
+    upper_date?: any;
+    message_file?: {
+        _id?: string,
+        public_id?: string,
+        mimeType?: string,
+        uri?: string,
+        height?: Number,
+        width?: Number,
+    };
+}
+
+// export MessageCardProps
+export interface MessageCardProps extends MessageProps {
+    sender_id?: string;
+    onMessagePress?: () => void;
+    upper_date?: string;
+    [key: string]: any;
+}
+
+// Send Message Props
+export interface SendMessageProps extends MessageProps {
+    delivered?: boolean;
+}
+
 // PickerItem Props
 export interface PickerItemProps {
     label: string;
@@ -226,3 +322,29 @@ export interface PickerProps {
     error?: string;
     containerStyle?: StyleProp<ViewStyle>;
 }
+
+// RoundIconButtonProps interface
+export interface RoundIconButtonProps extends AppIconTypes {
+    style?: StyleProp<ViewStyle>;
+}
+
+// RippleIconButtonProps interface
+export interface RippleIconButtonProps extends AppIconTypes {
+    style?: StyleProp<ViewStyle>;
+    onPress?: () => void;
+    rippleStyle?: StyleProp<ViewStyle>;
+}
+
+// audio player hook
+export interface AudioPlayerHookProps {
+    [key: string]: any;
+}
+
+export interface VideoPlayerHookProps {
+    AnimateTo?: any,
+    loadOnMount?: boolean,
+    autoPlay?: boolean,
+    source?: AVPlaybackSource | null,
+    isLooping?: boolean,
+    otherLoadProps?: AVPlaybackStatusToSet
+};
---
+++ types/ConfigTypes.ts
@@ -0,0 +1,76 @@
+// Interface for the config
+export interface ConfigProps {
+    // App Defaults
+    application_name?: string;
+    application_tag_line?: string;
+    default_profile_picture?: string;
+    JWT_Key?: string;
+
+    // Network Variables
+    __DEV__?: boolean;
+    baseUrl?: string;
+    DEV_BASE_URL?: string;
+    PROD_BASE_URL?: string;
+    api_version?: string;
+
+    // Main Prefixes Endpoints
+    auth_route?: string;
+    requirements_route?: string;
+    raisedhands_route?: string;
+    chats_route?: string;
+    otp_route?: string;
+    buysell_route?: string;
+    lostfound_route?: string;
+
+    // Auth Endpoints
+    editProfile?: string;
+    login?: string;
+    change_password?: string;
+    google_login?: string;
+    register?: string;
+    logout?: string;
+    toggle_push_notif?: string;
+    send_email_verify_otp?: string;
+    send_password_reset_otp?: string;
+    reset_password?: string;
+
+    // BuySell Endpoints
+    create_buy_sell?: string;
+    edit_buysell_item?: string;
+    get_own_buy_sell?: string;
+    delete_buy_sell?: string;
+    get_buysell_details?: string;
+    get_buy_sell_feed?: string;
+    search_buy_sell?: string;
+
+    // Lost Found Endpoints
+    create_lost_found?: string;
+    get_own_lostfound?: string;
+    delete_lostfound?: string;
+    get_lostfound_details?: string;
+    edit_lostfound?: string;
+    get_lostfound_feed?: string;
+    search_lostfound?: string;
+
+    // OTP Endpoints
+    verify_otp?: string;
+
+    // RaisedHands Endpoints
+    raise_a_hand?: string;
+    get_raisedHands_list?: string;
+    accept_raised_hand?: string;
+    reject_raised_hand?: string;
+
+    // Requirements Endpoints
+    new_requirement?: string;
+    get_own_requirements?: string;
+    edit_requirement?: string;
+    delete_requirement?: string;
+    requirement_feed?: string;
+
+    // google sign in variables
+    googleClientID?: string;
+
+    // Any other variables
+    [key: string]: any;
+}
