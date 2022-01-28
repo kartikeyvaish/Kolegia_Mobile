36e2e4db1bb5b6a5b175915ab854e061be6032e3
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-28 14:37:50 +0530

    :zap: app entry code cleanup

--- App.tsx
+++ App.tsx
@@ -1,73 +1,19 @@
-// packages imports
-import { connect } from "react-redux";
-import { NavigationContainer } from "@react-navigation/native";
-import { Provider } from "react-native-paper";
-
 // Components/Screens imports
 import AppNavigator from "./navigation/AppNavigator";
-import AuthActionCreators from "./store/auth/actions";
-import GlobalContext from "./contexts/GlobalContext";
-import OfflineNotice from "./components/OfflineNotice";
-import Overlay from "./components/OverlayModal";
-import ThemeActionCreators from "./store/theme/actions";
-import useModalOverlay from "./hooks/useModalOverlay";
-import useThemeManager from "./hooks/useThemeManager";
+import GlobalProvider from "./providers/GlobalProvider";
 // import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // AsyncStorage.clear();
 
 // App function component
-function App(props) {
-  // Destructure props
-  const { ToggleMode, Theme, User, SetUser } = props;
-
-  // Use modal custom hook
-  const modal_props = useModalOverlay();
-
-  // Light/Dark Mode manager using custom hook
-  useThemeManager(ToggleMode, Theme, true);
-
-  // Global Provider Value
-  const provider_value = {
-    User: User,
-    SetUser: SetUser,
-    ToggleMode: ToggleMode,
-    ...modal_props,
-  };
-
-  // Render component based on user authentication status
+function App() {
+  // render
   return (
-    <GlobalContext.Provider value={provider_value}>
-      <NavigationContainer theme={Theme}>
-        <Provider>
-          <Overlay
-            visible={modal_props.OverlayVisible}
-            text={modal_props.Text}
-          />
-          <AppNavigator />
-        </Provider>
-        <OfflineNotice />
-      </NavigationContainer>
-    </GlobalContext.Provider>
+    <GlobalProvider>
+      <AppNavigator />
+    </GlobalProvider>
   );
 }
 
-// Redux store that holds the states
-const mapStateToProps = (state) => {
-  return {
-    Theme: state.ThemeState.Theme,
-    User: state.AuthState.User,
-  };
-};
-
-// Dispatchers that will change the states
-const mapDispatchToProps = (dispatch) => {
-  return {
-    ToggleMode: (colorScheme) =>
-      dispatch(ThemeActionCreators.ChangeMode(colorScheme)),
-    SetUser: (user: any) => dispatch(AuthActionCreators.Login(user)),
-  };
-};
-
-// Exporting the App component with the states and dispatchers
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// exports
+export default App;
