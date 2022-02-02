01d94c520cd2bce977870fbf1c0b18a417762c1f
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-02-02 18:35:22 +0530

    :hammer: providers refractored

--- providers/GlobalProvider.tsx
+++ providers/GlobalProvider.tsx
@@ -7,8 +7,6 @@ import GlobalContext from "../contexts/GlobalContext";
 import OfflineNotice from "../components/OfflineNotice";
 import Overlay from "../components/OverlayModal";
 import ThemeProvider from "./ThemeProvider";
-import ThemeActionCreators from "../store/theme/actions";
-import useThemeManager from "../hooks/useThemeManager";
 import useModalOverlay from "../hooks/useModalOverlay";
 
 // import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -17,28 +15,23 @@ import useModalOverlay from "../hooks/useModalOverlay";
 
 interface GlobalProviderProps {
   children?: any;
-  ToggleMode?: any;
   User?: any;
   SetUser?: any;
   GlobalState?: any;
   modal_props?: any;
-  Theme?: any;
 }
 
 // GlobalProvider function component
 function GlobalProvider(props: GlobalProviderProps) {
   // Destructure props
-  const { GlobalState, children, Theme, ToggleMode, ...otherProps } = props;
+  const { GlobalState, children, User, ...otherProps } = props;
 
   // Use modal custom hook
   const modal_props = useModalOverlay();
 
-  // Light/Dark Mode manager using custom hook
-  useThemeManager(ToggleMode, Theme, true);
-
   // Global Provider Value
   const provider_value = {
-    ToggleMode,
+    User,
     ...GlobalState,
     ...modal_props,
     ...otherProps,
@@ -47,7 +40,7 @@ function GlobalProvider(props: GlobalProviderProps) {
   // Render component based on user authentication status
   return (
     <GlobalContext.Provider value={provider_value}>
-      <ThemeProvider theme={Theme}>
+      <ThemeProvider>
         <Overlay {...modal_props} />
         {children}
         <OfflineNotice />
@@ -68,8 +61,6 @@ const mapStateToProps = (state) => {
 // Dispatchers that will change the states
 const mapDispatchToProps = (dispatch) => {
   return {
-    ToggleMode: (colorScheme) =>
-      dispatch(ThemeActionCreators.ChangeMode(colorScheme)),
     SetUser: (user: any) => dispatch(AuthActionCreators.Login(user)),
   };
 };
--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,16 +1,44 @@
 // Packages Imports
 import { NavigationContainer } from "@react-navigation/native";
+import { connect } from "react-redux";
+
+// Local imports
 import PaperProvider from "./PaperProvider";
+import ThemeActionCreators from "../store/theme/actions";
+import useThemeManager from "../hooks/useThemeManager";
 
 // ThemeProvider function component
-function ThemeProvider({ theme, children }) {
+function ThemeProvider({ ToggleMode, children, Theme }) {
+  // Light/Dark Mode manager using custom hook
+  useThemeManager(ToggleMode, Theme, true);
+
   // Render
   return (
-    <NavigationContainer theme={theme}>
-      <PaperProvider theme={theme}>{children}</PaperProvider>
+    <NavigationContainer
+      theme={Theme}
+      linking={{
+        prefixes: ["https://kolegia.com", "kolegia://"],
+      }}
+    >
+      <PaperProvider theme={Theme}>{children}</PaperProvider>
     </NavigationContainer>
   );
 }
 
-// exports
-export default ThemeProvider;
+// Redux store that holds the states
+const mapStateToProps = (state) => {
+  return {
+    Theme: state.ThemeState.Theme,
+  };
+};
+
+// Dispatchers that will change the states
+const mapDispatchToProps = (dispatch) => {
+  return {
+    ToggleMode: (colorScheme: any) =>
+      dispatch(ThemeActionCreators.ChangeMode(colorScheme)),
+  };
+};
+
+// Exporting the GlobalProvider component with the states and dispatchers
+export default connect(mapStateToProps, mapDispatchToProps)(ThemeProvider);
