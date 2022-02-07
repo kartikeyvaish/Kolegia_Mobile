308066d451dd08e3f44075ce31116dec071352d4
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-02-07 14:56:26 +0530

    :sparkles: integrated push notifications

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // Packages Imports
 import "react-native-gesture-handler";
-import React from "react";
 import { AppRegistry } from "react-native";
+import messaging from "@react-native-firebase/messaging";
 import { Provider } from "react-redux";
+import PushNotification, { Importance } from "react-native-push-notification";
 import { PersistGate } from "redux-persist/integration/react";
 
 // Local Files/App/Components/Store import
 import App from "./App";
+import configurations from "./config/config";
 import { store, persistor } from "./store/configureStore";
+
+PushNotification.createChannel({
+  channelId: configurations.default_channel_id,
+  channelName: configurations.default_channel_id,
+  channelDescription: "A channel to show notifications",
+  playSound: true,
+  soundName: "default",
+  importance: Importance.HIGH,
+  vibrate: true,
+});
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {});
 
 // Headless Check for PushNotifications
 function HeadlessCheck({ isHeadless }) {
